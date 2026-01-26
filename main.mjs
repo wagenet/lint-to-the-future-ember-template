9ff36d035a92f0c69eefa48d6acff0fd445c4bed
Chris Manson <chris@manson.ie>
2026-01-26 17:28:05 +0000

    extract common getExistingIgnores function

--- main.mjs
+++ main.mjs
@@ -3,6 +3,7 @@ import { join } from 'path';
 import getFiles from './lib/get-files.js';
 import { Preprocessor } from 'content-tag';
 import debugBase from 'debug';
+import { getExistingIgnores } from './lib/utils.js';
 
 let p = new Preprocessor();
 const debug = debugBase('lint-to-the-future-ember-template');
@@ -10,22 +11,6 @@ const debug = debugBase('lint-to-the-future-ember-template');
 export { default as ignoreAll } from './lib/ignore.js';
 
 export const capabilities = ['filter-ignore'];
-
-function getIgnores(template) {
-  const firstLine = template.split('\n')[0];
-  if (!firstLine.includes('template-lint-disable')) {
-    return [];
-  }
-
-  const matched = firstLine.match(/template-lint-disable(.*)(--)?\}\}/);
-
-  const ignoreRules = matched[1].split(' ')
-    .map(item => item.trim())
-    // remove trailing -- from when there is no gaps in comments
-    .map(item => item.replace(/--$/, ''))
-    .filter(item => item.length);
-  return ignoreRules;
-}
 
 export async function list(directory) {
   // this is only used for internal testing, lint-to-the-future never passes a
@@ -45,14 +30,14 @@ export async function list(directory) {
       try {
       let templates = p.parse(file);
 
-      ignoreRules = templates.map(template => getIgnores(template.contents.trim())).flat();
+      ignoreRules = templates.map(template => getExistingIgnores(template.contents.trim())).flat();
       } catch (error) {
         console.warn("Unable to parse file", filePath);
         debug(error);
         ignoreRules = [];
       }
     } else {
-      ignoreRules = getIgnores(file);
+      ignoreRules = getExistingIgnores(file);
     }
 
 
